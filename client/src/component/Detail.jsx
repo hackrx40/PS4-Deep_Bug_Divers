7cbe0a8fbca5701e6ed26161324e6adfd4df41fe
Koruto <flyingshadow03@gmail.com>
2023-07-22 09:44:52 +0530

    Added Colour

--- client/src/component/Detail.jsx
+++ client/src/component/Detail.jsx
@@ -10,7 +10,7 @@ const Detail = () => {
       <h1 className="border-2 px-4 py-2 rounded-md bg-slate-100 hover:bg-slate-300">
         Stock Details: {stockName}
       </h1>
-      <RealtimeStock />
+      <RealtimeStock stockName={{ stockName }} />
     </div>
   );
 };
--- client/src/component/RealTimeStock.jsx
+++ client/src/component/RealTimeStock.jsx
@@ -8,7 +8,7 @@ import {
 } from 'recharts';
 import { useEffect, useState } from 'react';
 
-function App() {
+function App(props) {
   const [data, setData] = useState(null);
   //   const json = useRef([]);
 
@@ -19,10 +19,10 @@ function App() {
         const response = await fetch('http://localhost:3000/api/data');
         // console.log(savior);
         const jsonData = await response.json(); // Convert response to JSON
-        console.log(response, jsonData);
+        console.log(response, jsonData, props.stockName);
         // json.current = jsonData['3'];
         // console.log(json.current);
-        setData(jsonData['3']); // Extract the specific key value from the JSON data
+        setData(jsonData[props.stockName.stockName]); // Extract the specific key value from the JSON data
       } catch (error) {
         console.error('Error fetching JSON data:', error);
       }
@@ -33,6 +33,14 @@ function App() {
     return () => clearInterval(timer); // Clear the timer on component unmount
   }, []);
 
+  const getStrokeColor = () => {
+    if (data && data.length >= 2) {
+      const firstValue = data[0].amount;
+      const lastValue = data[data.length - 1].amount;
+      return lastValue < firstValue ? '#FF0000' : '#50C878';
+    }
+    return '#8884d8'; // Default color
+  };
   return (
     <div className="bg-white m-5">
       <LineChart
@@ -41,7 +49,8 @@ function App() {
         data={data}
         margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
       >
-        <Line type="natural" dataKey="value" stroke="#8884d8" />
+        <Line type="natural" dataKey="amount" stroke={getStrokeColor()} />
+
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="time" />
         <YAxis type="number" domain={[0, 900]} />
